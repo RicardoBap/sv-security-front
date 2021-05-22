afd3ff5164fe9774bbb43e071d52ca3ab9a7d4a7
RicardoBap <ric_bap@hotmail.com>
2021-05-22 16:25:10 -0300

    Configurando app-routing-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { HttpClientModule } from '@angular/common/http';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+
 import { CoreModule } from './core/core.module';
-
-import { PaginaNaoEncontradaComponent } from './core/pagina-nao-encontrada.component';
-
-import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
-import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
-import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
-import { LoginFormComponent } from './seguranca/login-form/login-form.component';
+import { AppRoutingModule } from './app-routing.module';
 
 import { ErrorInterceptorProvider } from './seguranca/interceptors/error-interceptor';
 import { AuthInterceptorProvider } from './seguranca/interceptors/auth-interceptor';
-
-
-const routes: Routes = [
-  { path: '', redirectTo: 'lancamentos', pathMatch: 'full' },
-
-  { path: 'lancamentos', component: LancamentosPesquisaComponent },
-  { path: 'lancamentos/novo', component: LancamentoCadastroComponent },
-  { path: 'lancamentos/:codigo', component: LancamentoCadastroComponent },
-  { path: 'pessoas', component: PessoasPesquisaComponent },
-  { path: 'login', component: LoginFormComponent },
-
-  { path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent },
-  { path: '**', redirectTo: 'pagina-nao-encontrada' }
-]
 
 @NgModule({
   declarations: [
@@ -43,7 +20,7 @@ const routes: Routes = [
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot(routes),
+    AppRoutingModule,
 
     CoreModule
   ],
